3ab4f6d5fd5edad69dde6e5a85f8b9c1bf996111
meric-coder <meric.kabo@gmail.com>
2026-06-11 17:38:58 +0300

    Revert "add timeout and new test"
    This reverts commit 2d0654f3a111adc2b39c9ab0c34479a07e94448f.

--- tests/apple.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('apple com', async ({ page }) => {
-await page.goto('https://www.apple.com/');
-await page.getByLabel('Store', { exact: true }).click();
-await page.getByLabel('Product', { exact: true }).getByRole('link', { name: 'iPhone' }).click();
-await expect(page.locator('h1')).toMatchAriaSnapshot(`- heading "Shop iPhone" [level=1]`);
-});
